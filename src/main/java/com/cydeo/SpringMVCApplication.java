d7ab7ddb8327d126472ed43d5253a336f24b63f4
fai7h01 <l.varsimashvil10@gmail.com>
2024-04-04 16:19:52 +0400

    Lab Controller

--- src/main/java/com/cydeo/SpringMVCApplication.java
+++ src/main/java/com/cydeo/SpringMVCApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Lab05SpringMvcApplication {
+public class SpringMVCApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Lab05SpringMvcApplication.class, args);
+        SpringApplication.run(SpringMVCApplication.class, args);
     }
 
 }
